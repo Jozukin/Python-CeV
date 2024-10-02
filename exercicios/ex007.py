aca7794d8550b3324ddd759b3cd00498211eaefb
João Victor Martins <jvmartinslima2@gmail.com>
2024-10-01 21:03:08 -0300

    média

---
+++ exercicios/ex007.py
@@ -0,0 +1,5 @@
+n1 = float(input('Qual foi a nota 1? '))
+n2 = float(input('Qual foi a nota 2? '))
+n3 = float(input('Qual foi a nota 3? '))
+med = (n1 + n2 + n3)/3
+print(f'Sua média final é: {med:.2f}')
