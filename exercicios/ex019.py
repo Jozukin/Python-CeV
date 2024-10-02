c134d76aac2b5ec40fbaac09b156eecabf0a9db0
João Victor Martins <jvmartinslima2@gmail.com>
2024-10-01 21:24:15 -0300

    sorteio 4 alunos

---
+++ exercicios/ex019.py
@@ -0,0 +1,7 @@
+import random
+aluno1 = input('Digite o nome do aluno 1: ')
+aluno2 = input('Digite o nome do aluno 2: ')
+aluno3 = input('Digite o nome do aluno 3: ')
+aluno4 = input('Digite o nome do aluno 4: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+print(f'O aluno escolhido foi: {random.choice(lista)}')
