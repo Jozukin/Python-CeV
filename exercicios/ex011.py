d100fa0382309956bc64ab793da304f25067fac2
João Victor Martins <jvmartinslima2@gmail.com>
2024-10-01 21:04:54 -0300

    área

---
+++ exercicios/ex011.py
@@ -0,0 +1,5 @@
+b = float(input('Qual a largura em metros da sua parede? '))
+h = float(input('Qual a altura em metros da sua parede? '))
+a = b * h
+t = a / 2
+print(f'A área da sua parede é {a}m² e a quantidade de tinta necessária é {t}L')
