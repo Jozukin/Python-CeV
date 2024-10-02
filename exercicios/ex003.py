a94c66167626d1d27d8011e3b1ade7c34f152f30
João Victor Martins <jvmartinslima2@gmail.com>
2024-10-01 21:01:54 -0300

    calculadora somar 2 números

---
+++ exercicios/ex003.py
@@ -0,0 +1,6 @@
+print('Calculadora para somar 2 números')
+n1 = int(input('Digite o primeiro número '))
+n2 = int(input('Digite o segundo número '))
+s = n1+n2
+# print('A soma de {} e {} é igual a {}'.format(n1, n2, s))
+print(f'A soma de {n1} e {n2} é igual a {s}')
