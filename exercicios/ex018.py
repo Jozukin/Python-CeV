5a8268aa03cf61365ee5fde68f6feeae65651aac
João Victor Martins <jvmartinslima2@gmail.com>
2024-10-01 21:07:48 -0300

    seno, cosseno e tangente

---
+++ exercicios/ex018.py
@@ -0,0 +1,5 @@
+import math
+n = float(input('Digite um ângulo: '))
+print(f'O cosseno do ângulo {n}° é {math.cos(math.radians(n))}')
+print(f'O seno do ângulo {n}° é {math.sin(math.radians(n))}')
+print(f'A tangente do ângulo {n}° é {math.tan(math.radians(n))}')
