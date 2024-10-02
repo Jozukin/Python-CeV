dd1420e644c5a21bf78b2d7473ea20f9ef120bab
João Victor Martins <jvmartinslima2@gmail.com>
2024-10-02 01:24:48 -0300

    módulo moeda formatado

---
+++ exercicios/ex109.py
@@ -0,0 +1,29 @@
+from modulos.moeda import aumentar, reduzir, dobro, metade, fmoeda
+porc = 0
+valor = float(input('Digite o preço: R$'))
+while True:
+    print('''Digite a opção que deseja calcular:
+    [ A ] Aumento
+    [ R ] Redução
+    [ N ] Não desejo calcular''')
+    op = input('Sua escolha: ')
+    if op in 'AaRrNn':
+        if op in 'Aa':
+            while True:
+                porc = input('Digite o valor da porcentagem do seu aumento: ')
+                if porc.isnumeric():
+                    porc = float(porc)
+                    break
+        elif op in 'Rr':
+            while True:
+                porc = input('Digite o valor da porcentagem da sua redução: ')
+                if porc.isnumeric():
+                    porc = float(porc)
+                    break
+        break
+print(f'A metade de {fmoeda(valor)} é {metade(valor, formatar=True)}')
+print(f'O dobro de {fmoeda(valor)} é {dobro(valor, formatar=True)}')
+if op in 'Aa':
+    print(f'O valor {fmoeda(valor)} com o aumento de {porc}% é {aumentar(valor, porc, formatar=True)}')
+elif op in 'Rr':
+    print(f'O valor {fmoeda(valor)} com a redução de {porc}% é {reduzir(valor, porc, formatar=True)}')
